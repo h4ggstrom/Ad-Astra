7c7d3e10989099de37cffc786221c311121a2a1a
h4ggstrom <robin2angelis@gmail.com>
2024-09-26 09:48:34 +0200

    random comments

--- bot.py
+++ bot.py
@@ -11,18 +11,21 @@ Je pense ca va partir sur un gros tuto ytb un de ces jours, et inchallah ca va b
 import discord
 import config as cfg
 
-# Define the intents
+# the bot needs to read commands (but only commands, not all messages), and tag users, if needed
 intents = discord.Intents.default()
+intents.message_content = False
 intents.messages = True
 intents.members = True
 
 # creating the client
-client = discord.Client(intents=intents)
+client = discord.Client(intents=intents) # intents on the left refers to the Client class variable, intents on the right refers to the parameter
 
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user}')
-    
+   
+
+# tbh this try/catch section is useless considering the token doesn't expire (and works), but let's call that *code quality* :upside_down:
 try:
     client.run(cfg.DISCORD_TOKEN)
 except discord.errors.LoginFailure:
